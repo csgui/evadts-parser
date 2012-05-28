58f5f1971ca715e7c01ae3c5916b0270d9f289f7
Christian Guimaraes <cguimaraes.br@gmail.com>
2012-05-28 14:37:53 +0100

    Grouping multimethods

--- src/parser/core.clj
+++ src/parser/core.clj
@@ -1,31 +1,7 @@
 (ns parser.core
   (:require [clojure.java.io :as io]
             [clojure.string  :as str])
-  (:use [parser.handlers.dxs]
-        [parser.handlers.st]
-        [parser.handlers.id1]
-        [parser.handlers.cb1]
-        [parser.handlers.va1]))
-
-(defmulti retrieve-group-handler (fn [x] x) :default :unknown-group-handler)
-
-(defmethod retrieve-group-handler :DXS [_]
-  handle-dxs)
-
-(defmethod retrieve-group-handler :ST [_]
-  handle-st)
-
-(defmethod retrieve-group-handler :ID1 [_]
-  handle-id1)
-
-(defmethod retrieve-group-handler :CB1 [_]
-  handle-cb1)
-
-(defmethod retrieve-group-handler :VA1 [_]
-  handle-va1)
-
-(defmethod retrieve-group-handler :unknown-group-handler [_]
-  nil)
+  (:use [parser.group-handler]))
 
 (defn get-group-identifier [line]
   (keyword (first (str/split line #"\*"))))
--- src/parser/group_handler.clj
+++ src/parser/group_handler.clj
@@ -1,9 +1,26 @@
 (ns parser.group-handler
   (:use [parser.handlers.dxs]
         [parser.handlers.st]
-        [parser.handlers.id1]))
+        [parser.handlers.id1]
+        [parser.handlers.cb1]
+        [parser.handlers.va1]))
 
-(def group-handlers
-  {"DXS" handle-dxs,
-   "ST"  handle-st,
-   "ID1" handle-id1})
+(defmulti retrieve-group-handler (fn [x] x) :default :unknown-group-handler)
+
+(defmethod retrieve-group-handler :DXS [_]
+  handle-dxs)
+
+(defmethod retrieve-group-handler :ST [_]
+  handle-st)
+
+(defmethod retrieve-group-handler :ID1 [_]
+  handle-id1)
+
+(defmethod retrieve-group-handler :CB1 [_]
+  handle-cb1)
+
+(defmethod retrieve-group-handler :VA1 [_]
+  handle-va1)
+
+(defmethod retrieve-group-handler :unknown-group-handler [_]
+  nil)
